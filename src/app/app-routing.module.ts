600469f87fb662b85e9575dd8030d56b3db9efd7
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-25 09:35:00 -0500

    Se añadio Form Login y Servicios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent}, //redireccionar al Home
+  {path:'login',component:LoginComponent}, //redireccionar al Login
+  { path: '**', pathMatch:'full', redirectTo: 'login' }// cualquier url redirigir a login
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/services/permisos.service.ts
@@ -0,0 +1,29 @@
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PermisosService {
+  baseUrl: string = 'https://jsonplaceholder.typicode.com/users' //Url de la Api
+  usuariosList: any[] = []; //almacena el usuarios obtenido por el filtrado
+  constructor(private http:HttpClient) { }
+
+  //Método para obtener los Usuarios
+  getUsuarios(){
+    return this.http.get<any>(`${this.baseUrl}`)//esto nos devuelve la información de los usuarios
+  }
+
+  //filtrar usuario por username y por email
+  loginUsuario(username:string , email:string){
+    return this.http.get<any>(`${this.baseUrl}`)
+      .pipe(
+        map(usuarios => {
+          this.usuariosList = usuarios.filter((usuario:any) => usuario.username === username && usuario.email === email);
+          return this.usuariosList;
+        })
+      );
+  }
+}
