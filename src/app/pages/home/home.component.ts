ab75f99b0a6959a18928f9ef677746929e32deb1
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-25 10:44:11 -0500

    Se muestra Lista de Usuarios y creación de Interface Usuarios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuarios } from 'src/app/interfaces/usuarios';
+import { PermisosService } from 'src/app/services/permisos.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  users !: Usuarios[]; //declaramos un array utilizando nuestra interface Usuarios
+  constructor(private usuariosService:PermisosService) {
+    this.getUsuarios(); //llamamos al método traer Usuarios
   }
 
+  ngOnInit(): void {
+
+  }
+
+  Logout(){
+    this.usuariosService.logout(); //cerrar sesión
+  }
+
+  getUsuarios(){
+    //utilizando nuestro método getUsuarios traemos todos los usuarios del Json y lo almacenamos en nuestro Array users
+    this.usuariosService.getUsuarios().subscribe(
+      (data: Usuarios[]) => {
+        this.users = data;
+      }
+    );
+  }
 }
--- src/app/services/permisos.service.ts
+++ src/app/services/permisos.service.ts
@@ -1,7 +1,9 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { map, Observable } from 'rxjs';
+import { Usuarios } from '../interfaces/usuarios';
 
 
 @Injectable({
@@ -10,11 +12,13 @@ import { map, Observable } from 'rxjs';
 export class PermisosService {
   baseUrl: string = 'https://jsonplaceholder.typicode.com/users' //Url de la Api
   usuariosList: any[] = []; //almacena el usuarios obtenido por el filtrado
-  constructor(private http:HttpClient) { }
+  private isLogin!:boolean; //validara si el usuario inicio Sesión
+
+  constructor(private http:HttpClient, private router:Router) { }
 
   //Método para obtener los Usuarios
   getUsuarios(){
-    return this.http.get<any>(`${this.baseUrl}`)//esto nos devuelve la información de los usuarios
+    return this.http.get<Usuarios[]>(`${this.baseUrl}`)//esto nos devuelve la información de los usuarios
   }
 
   //filtrar usuario por username y por email
@@ -23,8 +27,19 @@ export class PermisosService {
       .pipe(
         map(usuarios => {
           this.usuariosList = usuarios.filter((usuario:any) => usuario.username === username && usuario.email === email);
+          //solo si se encontró un usuario con esas credenciales se asigna true a la variable isLogin
+          if(this.usuariosList.length > 0){
+            this.isLogin = true;
+          }
           return this.usuariosList;
         })
       );
+  }
+
+  //si el usuario cierra sesión redireccionar al Login
+  logout() {
+    this.isLogin = false;
+    // Redirigir a la página de inicio de sesión
+    this.router.navigate(['/login']);
   }
 }
