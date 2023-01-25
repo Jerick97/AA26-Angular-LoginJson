52915abf9fe2073f66e3747904547dc4c808dcf5
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-25 10:03:04 -0500

    se agrego validación Login y route

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PermisosService } from 'src/app/services/permisos.service';
+// ES6 Modules or TypeScript
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public formLogin !: FormGroup;
+  constructor( private usuarioService:PermisosService, private router:Router, private formBuilder:FormBuilder) { 
+    
   }
 
+  ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      username:['',[
+        Validators.required,
+        Validators.minLength(4)
+      ]] ,
+      password:['',[
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)
+      ]]
+    })
+  }
+
+  onSubmit(){
+    this.usuarioService.loginUsuario(this.formLogin.get('username')?.value,this.formLogin.get('password')?.value)
+    .subscribe(
+      (usuarios) => {
+        if (usuarios.length > 0) {
+          // Usuario autenticado correctamente, mostrar alerta de éxito
+          Swal.fire({
+            icon: 'success',
+            title: 'Autenticado',
+            text: 'Ingreso Exitoso'
+          })
+          this.router.navigate(['/home']);
+        } else {
+          // Usuario o contraseña incorrectos, mostrar alerta de error
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Usuario o contraseña incorrectos'
+          })
+        }
+      }
+    );
 }
+}
+
--- src/app/services/permisos.service.ts
+++ src/app/services/permisos.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
